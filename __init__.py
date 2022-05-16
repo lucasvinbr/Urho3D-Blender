5816423f3c73af38e5cacee94b001b44cebffff6
Lucas Vinicius <lucasvinbr@gmail.com>
2022-05-16 02:25:09 -0300

    Fix for error on bad UV vertices' highlighting

--- __init__.py
+++ __init__.py
@@ -1299,7 +1299,7 @@ def selectVertices(context, objectName, indicesList, deselect):
         return
 
     # Set the object as current
-    objects.active = obj
+    bpy.context.view_layer.objects.active = obj
     # Enter Edit mode (check poll() to avoid exception)
     if bpy.ops.object.mode_set.poll():
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
